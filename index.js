aa18916af2fb74d6de1acb942c4ba326672de0de
Eche Oji <echyth@yahoo.com>
2022-07-09 11:25:33 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note.js");
 
 const app = express();
 app.set("json spaces", 2);
 app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
 
 const requestLogger = (request, response, next) => {
   console.table(`Method: ${request.method}, Path: ${request.path}`);
@@ -41,7 +22,8 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/notes", async (request, response) => {
+  const notes = await Note.find({});
   response.json(notes);
 });
 
@@ -86,7 +68,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ models/note.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// const url = process.env.MONGODB_URI;
+const url = `mongodb+srv://echezona:FantasyTvigt1974@cluster0.xhujs.mongodb.net/note-app?retryWrites=true&w=majority`;
+console.log("Connecting to database");
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB");
+    console.error(error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
