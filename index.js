632925611b5e42f2dc8e38c7d50645acf16b47cb
Eche Oji <echyth@yahoo.com>
2022-07-09 13:12:44 +0100

    uibuild

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ app.get("/api/notes", async (request, response) => {
   response.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((n) => n.id === Number(id));
-  if (note) {
-    return response.json(note);
+app.get("/api/notes/:id", async (request, response) => {
+  const savedNote = await Note.findById(request.params.id);
+
+  if (savedNote) {
+    return response.json(savedNote);
   }
 
   response.status(404).end();
@@ -44,22 +44,21 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", async (request, response) => {
   if (!request.body.content) {
     return response.status(400).json({ error: "content missing" });
   }
 
-  const { content, important } = request.body;
+  const body = request.body;
 
-  const newNote = {
-    content,
-    important: important || false,
-    id: generateId(),
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
     date: new Date(),
-  };
+  });
 
-  notes = notes.concat(newNote);
-  response.json(newNote);
+  const savedNote = await note.save();
+  response.json(savedNote);
 });
 
 const unknownEndpoint = (request, response) => {
